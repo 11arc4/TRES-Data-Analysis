c30bde6650b7e8c0c1e33e37c6d8b16768a4ae15
11arc4 <11arc4@queensu.ca>
2017-02-08 16:15:31 -0500

    Made some new plots of growth data

--- Growth Rate Prelim Modeling.R
+++ Growth Rate Prelim Modeling.R
@@ -1,5 +1,7 @@
 # Nestling Growth Rate analysis
 library(dplyr)
+library(ggplot2)
+library(scatterplot3d)
 
 inputdir<- "~/Masters Thesis Project/Tree Swallow Data/Amelia TRES data 1975-2016/Extracted Data for Analysis"
 filename <- "Nestling Growth Rate Modeling Data.csv"
@@ -20,9 +22,17 @@ gdata2 <- gdata [which(gdata$growthrate_mass> (-5) & gdata$growthrate_mass< (10)
 gmdata <- gdata2[which(!is.na(gdata2$hatchdate) & !is.na(gdata2$hatchsize)), ]
 plot (growthrate_mass~ year, data=gmdata) #That's way better!! No super wonky points anymore
 
+ggplot(gmdata, aes(x=year, y= growthrate_mass))+
+  geom_point(alpha= .5, position="jitter", aes(color=hatchsize))+
+  xlab("Year") +
+  ylab("Growth Rate (g/day)")+
+  stat_smooth()
+
+
 #Model growth rate!
 mod <- lm ( growthrate_mass~ (year^2) * year*hatchdate*hatchsize, data = gmdata)
 summary(aov(mod))
+summary(mod)
 plot(mod) #Q-Q plot isn't that great
 hist(resid(mod)) #little left skewed but not too bad
 shapiro.test(resid(mod)) #significantly different from normal
@@ -37,9 +47,15 @@ plot(resid(mod)~gmdata$hatchsize)
 
 #Model Fledge success based on growth rate averages per nest 
 #Need to use averages per nest because otherwise it will be unclear which individuals fledges and which died
+FGrData <- summarise(group_by(.data=gmdata, nest), mean(year), mean(growthrate_mass, na.rm = TRUE), mean(hatchdate), mean(hatchsize), mean(fledgesize))
 
-FGrData <- summarise(group_by(gmdata, nest), mean(year), mean(growthrate_mass, na.rm = TRUE), mean(hatchdate), mean(hatchsize), mean(fledgesize))
+
+
+
 colnames(FGrData) <- c("nest", "year", "growthrate", "hatchdate", "hatchsize", "fledgesize")
+
+scatterplot3d(x= FGrData$year, y=FGrData$growthrate, z=FGrData$fledgesize)
+
 
 fmod <- lm(fledgesize ~ year* growthrate * hatchdate * hatchsize, data= FGrData)
 summary(aov(fmod))
