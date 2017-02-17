6cf3d6c46f8b66083122c520c4b6fb621879f226
11arc4 <11arc4@queensu.ca>
2017-02-17 12:05:22 -0500

    initial commit

---
+++ Pre-field experiment power analysis.R
@@ -0,0 +1,13 @@
+library(pwr)
+
+
+#Power analaysis 
+#I should have 70 nests
+pwr.t.test(power=0.8, n= 70, sig.level = 0.05)
+#d = 0.4768651 so can see medium effect sizes
+#For the 
+
+
+#For the nestlings (counting everyone )
+pwr.t.test(power=0.8, n= 840, sig.level = 0.05)
+#d = 0.1367796 can see small effect sizes
