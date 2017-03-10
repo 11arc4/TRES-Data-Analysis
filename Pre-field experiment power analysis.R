b8295ed5d6021216101b06fff8cfbef0138af644
11arc4 <11arc4@queensu.ca>
2017-03-10 09:08:26 -0500

    power analysis

--- Pre-field experiment power analysis.R
+++ Pre-field experiment power analysis.R
@@ -9,5 +9,9 @@ pwr.t.test(power=0.8, n= 70, sig.level = 0.05)
 
 
 #For the nestlings (counting everyone )
+
+
+pwr.t.test(power=0.8, d=.4, sig.level = 0.05)
+#We only need 100 nestlings to get medium sized effect sizes visible
 pwr.t.test(power=0.8, n= 840, sig.level = 0.05)
 #d = 0.1367796 can see small effect sizes
