cd34fd6ff34dd64e0cf10d7891d5366c6e2eb357
11arc4 <11arc4@queensu.ca>
2017-05-08 10:08:16 -0400

    new spacing.... silly thing to commit

--- Vital Rates Analysis.R
+++ Vital Rates Analysis.R
@@ -12,19 +12,19 @@
 # nestVR$ASYclutchsize[1:length(parametersASYF$clutch [which(!is.na(parametersASYF$clutch))])]  <- parametersASYF$clutch [which(!is.na(parametersASYF$clutch))]
 # nestVR$hatchrate[1:length( hatchPar$hatch/ hatchPar$clutch[which(!is.na(hatchPar$hatch/ hatchPar$clutch))])] <-  hatchPar$hatch/ hatchPar$clutch[which(!is.na(hatchPar$hatch/ hatchPar$clutch))]
 # nestVR$fledgerate[1:length(fledgePar$fledge/ fledgePar$hatch[which(!is.na(fledgePar$fledge/ fledgePar$hatch))])] <-  fledgePar$fledge/ fledgePar$hatch[which(!is.na(fledgePar$fledge/ fledgePar$hatch))]
-  
+library(popbio)
 
 write.csv(nestVR, file="~/Masters Thesis Project/TRES Data Analysis/Matrix Pop Estimates/Vital Rates.csv", row.names = F )
 nestVR <- read.csv("~/Masters Thesis Project/TRES Data Analysis/Matrix Pop Estimates/Vital Rates.csv", na.strings=c("NA"), as.is=T)
 
 
-vrdat <- as.data.frame(matrix(nrow=1000, ncol=13))
+vrdat <- as.data.frame(matrix(nrow=10000, ncol=13))
 colnames(vrdat)<- c("hatchrate", "fledgerate", "recruitrate", "eggRecruitRate", "SYrenests", "SYclutchsize", "SYlayrate", "ASYrenests", "ASYclutchsize", "ASYlayrate", "SYReturn", "ASYReturn", "lambda" )
 
 stages <- c("egg", "SY", "ASY")
 A <- matrix(0, nrow=3, ncol=3, dimnames = list(stages, stages))
 N0 <- c(0, 16 , 38)
-for (i in 1:1000){
+for (i in 1:nrow(vrdat)){
   #Pull out a a random ratch rate, fledge rate, renest status and clutch size from the vectors I made above. 
   vrdat$hatchrate[i] <-  sample (nestVR$hatchrate[which(!is.na(nestVR$hatchrate))], 1)
   vrdat$fledgerate[i] <- sample (nestVR$fledgerate[which(!is.na(nestVR$fledgerate))], 1)
@@ -73,4 +73,101 @@ plot(lambda~recruitrate, data=vrdat) #There's so little variation in the recruit
 plot(lambda~hatchrate, data=vrdat) #I think this means that you need both hatchrate and fledgerate to be high
 plot(lambda~fledgerate, data=vrdat) 
 
+modASYR <- lm(lambda~ASYReturn, data=vrdat)
+plot(modASYR) #The Q-Q plot looks like shit
+hist(resid(modASYR)) #Oh those residuals really aren't normal even a little bit 
+#Oh it's that damn straight line. I have no idea why we have that..... #I guess it's because there's a hard minimum if we have no eggs recruit that year
+summary(modASYR)
+#R^2=0.002134 
 
+modSYR <- lm(lambda~SYReturn, data=vrdat)
+plot(modSYR)
+summary(modSYR)
+#R^2 = -6.094e-06 
+
+#Basically return rates aren't a big driver of lambda
+
+
+modSYlay <- lm(lambda~SYLayrate, data=vrdat)
+summary(modSYlay)dd
+#R^2 <0.01302
+
+modASYlay <- lm(lambda~ASYLayrate, data=vrdat)
+summary(modASYlay)
+#R^2 < 0.01423 
+
+modeggRecruit <- lm(lambda~eggRecruitRate, data=vrdat)
+summary(modRecruit)
+#R^2 = 0.924
+#OOOO there's an actual R^2. Let's investigate further. 
+
+
+modrecruit <- lm(lambda~recruitrate, data=vrdat)
+summary(modrecruit)
+#R^2= 0.002382
+
+modhatch <- lm(lambda~hatchrate, data=vrdat)
+summary(modhatch)
+#R^2 = 0.2675
+modfledge <- lm(lambda~fledgerate, data=vrdat)
+summary(modfledge)
+#R^2=0.5471 THIS IS THE MOSt IMPORTANT PARAMETER BY THIS ESTIMATE
+
+
+
+
+
+
+
+
+
+################################ I feel a bit weird about using nest level data
+#to parameterize the model since the fledge, hatch, and and all those rates are
+#nest level. Perhaps it would be better to calculate population level rates for
+#each of the years and then use that to parameterize the model
+yearlyParam <- as.data.frame(matrix(nrow=42, ncol=5))
+colnames(yearlyParam)<- c("year", "SYclutch", "ASYclutch", "hatchrate", "fledgerate")
+r=0
+for (y in 1975:2016){
+  r=r+1
+  yearly <- parameters %>% filter (year==y)
+  yearlyParam$year[r] <- y
+  yearlyParam$SYclutch[r]<- mean(yearly[which(yearly$FAge=="SY"), "clutch"], na.rm=T)
+  yearlyParam$ASYclutch[r]<- mean(yearly[which(yearly$FAge=="ASY"), "clutch"], na.rm=T)
+  
+  yearlyParam$hatchrate[r] <- mean(yearly[which(yearly$clutch>0 & !is.na(yearly$hatch)), "hatch" ] /
+                                     yearly[which(yearly$clutch>0 & !is.na(yearly$hatch)), "clutch" ])
+  yearlyParam$fledgerate[r] <- mean(yearly[which(yearly$hatch>0 & !is.na(yearly$fledge)), "fledge" ] /
+                                     yearly[which(yearly$hatch>0 & !is.na(yearly$fledge)), "hatch" ])
+}
+
+plot(SYclutch~year, data=yearlyParam)
+plot(ASYclutch~year, data=yearlyParam)
+plot(hatchrate~year, data=yearlyParam)
+plot(fledgerate~year, data=yearlyParam)
+
+
+hist(yearlyParam$hatchrate)
+abline(v=mean(yearlyParam$hatchrate), col="red")
+abline(v=mean(yearlyParam$hatchrate)+sd(yearlyParam$hatchrate), col="blue")
+abline(v=mean(yearlyParam$hatchrate)-sd(yearlyParam$hatchrate), col="blue")
+
+mean(yearlyParam$hatchrate)
+sd(yearlyParam$hatchrate)
+shapiro.test(yearlyParam$hatchrate) #it's normal
+
+hist(yearlyParam$fledgerate)
+abline(v=mean(yearlyParam$fledgerate), col="red")
+abline(v=mean(yearlyParam$fledgerate)+sd(yearlyParam$fledgerate), col="blue")
+abline(v=mean(yearlyParam$fledgerate)-sd(yearlyParam$fledgerate), col="blue")
+
+mean(yearlyParam$fledgerate)
+sd(yearlyParam$fledgerate)
+shapiro.test(yearlyParam$fledgerate) #it's normal
+
+
+library(fitdistrplus)
+descdist(yearlyParam$SYclutch[which(!is.na(yearlyParam$SYclutch))],  discrete=T) #looks like a negative binomial 
+rnbinom(n=1, size= ??????, mu=5.063771)
+
+#Hmmmm Unsure how to get the best estimates for clutch size. Maybe could I mix the methods and use population level 
